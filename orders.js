c3314c6da48821bf83f8aa9cf073abb75bc76b11
João Vitor Silva <93292132+joaoviitorsx@users.noreply.github.com>
2025-02-18 15:59:17 -0300

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Curso de JavaScript',
+        productNumber: '85743',
+        paymentStatus: 'Pendente',
+        status: 'Pendente'
+    },
+    {
+        productName: 'Curso de CSS Completo',
+        productNumber: '97245',
+        paymentStatus: 'Reembolsado',
+        status: 'Recusado'
+    },
+    {
+        productName: 'Curso de Desenvolvedor FullStack',
+        productNumber: '36452',
+        paymentStatus: 'Pagamento Confirmado',
+        status: 'Ativado'
+    },
+]
